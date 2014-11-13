4aebac470c5d7c111d32abc1a96919d1c6acd49b
Qiyuan Qiu <qiuqiyuan@gmail.com>
2014-11-13 02:20:02 -0500

    change SFrame construction method
    Change from:
    	sf =  SFrame()
    	use sf.append()
    to:
    	a bunch SArray()
    	....
    	sf.add_column()
    
    this is faster

--- PyDevAnalysis/src/main/main.py
+++ PyDevAnalysis/src/main/main.py
@@ -1,6 +1,7 @@
 __author__ = 'Qiyuan Qiu, Jeffery White'
 
 from graphlab import SFrame
+from graphlab import SArray
 import json
 
 n_total_lines = 220000
@@ -8,11 +9,30 @@ n_total_lines = 220000
 def main():
     with open('../../Data/data_file_modified.txt') as data:
         sf = SFrame()
+        dt = []
+        ip = []
+        py = []
+        script = []
+        id = []
         for i, line in enumerate(data):
-            new_row = SFrame(json.loads(line))
-            sf = sf.append(new_row)
-            if i % 10000 == 0:
+            jo = json.loads(line)
+            dt += jo['dt']
+            ip += jo['ip']
+            py += jo['py']
+            id += [i]
+            script += jo['user_script']
+
+            if i % 100 == 0:
                 print float(i) / n_total_lines
+            if i > 200:
+                break
+
+        sf = sf.add_column(SArray(id), name='id')
+        sf.add_column(SArray(dt), name='dt')
+        sf.add_column(SArray(ip), name='ip')
+        sf.add_column(SArray(py), name='py')
+        sf.add_column(SArray(script), name='user_script')
+
         sf.save('python_tutor')
         sf.show()
 
