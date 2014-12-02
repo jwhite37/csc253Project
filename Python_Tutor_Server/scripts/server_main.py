32408d35d50addb8d91271b1fd2ff79d406bcef7
jwhite37 <jwhite37@ur.rochester.edu>
2014-12-02 15:37:40 -0500

    Modifications for the server!

--- Python_Tutor_Server/scripts/server_main.py
+++ Python_Tutor_Server/scripts/server_main.py
@@ -1,6 +1,8 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import graphlab as gl
 from graphlab import SFrame
+import cgi
+import codediff
 
 SERVER_PORT = 8080
 
@@ -8,21 +10,61 @@ SERVER_PORT = 8080
 class PythonTutorSession:
 
 	#Initialize the session from the clean frames (our database in this case)
-	def __init__(self,version,session_id):
-		if(version == 2):
-			sf = graphlab_loadsframe("data/py2_session_clean")
+	def __init__(self,version,session_id,diff):
+		if(int(version) == 2):
+			sf = gl.load_sframe("data/py2_session_clean")
 		else:
-			sf = graphlab_load_sframe("data/py3_session_clean")
+			sf = gl.load_sframe("data/py3_session_clean")
 
-		self.session = sf.filter([session_id])
+		self.session = sf.filter_by([int(session_id)],"session_id")
+
+		if(diff == "Full"):
+			self.diffs = False
+		else:
+			self.diffs = True
 
 	#Return basic information about the session
 	def getSessionInformation(self):
-		return "<p>TESTING SESSION</p>"
+		m_session = self.session
+		information = m_session[1]
+
+		html = "<h2>Basic Session Information</h2><hr/>"
+		
+		html += "<p align='center'>"
+		html += "<b>Session ID:</b>&nbsp;" + str(information['session_id']) 
+		html += "&nbsp;&nbsp;&nbsp;<b>IP Address:</b>&nbsp;" + str(information['ip'])
+		html += "&nbsp;&nbsp;&nbsp;<b>Python:</b>&nbsp;" + str(information['py'])
+		html += "</p><hr/>"
+		
+		return html
 
 	#Return the diffs
 	def getDiffs(self):
-		return "<p>TESTING DIFFS</p>"
+		html = "<h2>Diff Data for this Session</h2>"
+
+		m_session = self.session
+		m_session.sort('dt')
+
+		print m_session.column_names()
+
+		prev_row = ""
+
+		for e in m_session:
+			if(prev_row == ""):
+				prev_row = e
+			else:
+				html += "<p align='center'><b>Submission Date:&nbsp;" + str(prev_row['dt']) + "&nbsp;&nbsp;with error:&nbsp;" + str(prev_row['err_msg'])
+				html += "<p align='center'><b>Submission Date:&nbsp;" + str(e['dt']) + "&nbsp;&nbsp;with error:&nbsp;" + str(e['err_msg'])
+
+				html += codediff.sdiff_lines(prev_row['user_script'].split('\n'),e['user_script'].split('\n'),prev_row['dt'],e['dt'],self.diffs,0,3)
+				html += "<hr/>"
+
+				prev_row = e
+		return html
+
+	#Return comment form
+	def getComment(self):
+		return "<p>COMMENT FORM!</p>"
 	
 #HTTP Server basic handler
 class PyDataAnalysisHandler(BaseHTTPRequestHandler):
@@ -38,9 +80,35 @@ class PyDataAnalysisHandler(BaseHTTPRequestHandler):
 		return
 	#Handling for HTTP POST Requests
 	def do_POST(self):
-		#Here we need to handle a post request containing a session ID
-		#and output the diffs for the submissions in the session
-		#(can we also do the AST graph?)
+		#Grab the incoming form data and send a successfully 'we get this' response along with the headers
+		web_form = cgi.FieldStorage(fp=self.rfile,
+					    headers=self.headers,
+                                            environ={'REQUEST_METHOD':'POST','CONTENT_TYPE':self.headers['Content-Type']})
+		self.send_response(200)
+		self.end_headers()
+
+		#Get the request type (in this we're embedding in the 'submit' button value
+		request = web_form.getvalue("submit")
+
+		#Handle the request as appropriate
+		if(request == "Query"):
+			version = web_form.getvalue("python_version")
+			session = web_form.getvalue("session_id")
+			diff = web_form.getvalue("diff_version")
+
+			m_session = PythonTutorSession(version,session,diff)
+
+			html_body = m_session.getSessionInformation()
+			html_body += m_session.getDiffs()
+			html_body += m_session.getComment()
+
+		elif(request == "Comment"):
+			html_body = "RECEIVED: Comment!"
+		else:
+			html_body = "BAD REQUEST!"
+
+		response = "<html><head></head><body>" + html_body +"</body></html>"
+		self.wfile.write(response)
 		return
 
 #Main method, create the server and let it run
